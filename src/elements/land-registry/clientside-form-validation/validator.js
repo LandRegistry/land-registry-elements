8a79c5ee02ceae98408abf8451b97948932a8286
Andy Mantell <andymantell@hotmail.com>
2016-02-12 14:13:23 +0000

    Further work on the clientside form validation

--- src/elements/land-registry/clientside-form-validation/validator.js
+++ src/elements/land-registry/clientside-form-validation/validator.js
@@ -1,9 +1,10 @@
 'use strict';
 
-require('validate-js');
+global.validate = require('validate.js'); // Expose validate as a global so that people can add custom validation routines easily
 var extend = require('extend');
 var domify = require('domify');
 var closest = require('closest');
+var delegate = require('delegate');
 
 /**
  * Form validation
@@ -53,24 +54,77 @@ function Validator(element, config) {
       return;
     }
 
-    // Set up our form validator instance
-    validatorInstance = new FormValidator(element, options.rules, errorHandler);
+    // Bind form submit handler
+    element.addEventListener('submit', submit);
+
+    // Set up form field keyup handlers
+    delegate(element, '.form-control', 'keyup', keyup);
 
   }
 
   /**
-   * Error handler
+   * Main validation helper
    */
-  function errorHandler(errors, event) {
+  function validateForm() {
+    var errors = validate(element, options.rules, {
+      fullMessages: true
+    });
+
+    var errorData = [];
+
+    // Pre-process the errors to be suitable for hogan
+    for(var key in errors) {
+      if(errors.hasOwnProperty(key)) {
+        errors[key].forEach(function(error) {
+          errorData.push({
+            'id': key,
+            'message': error
+          });
+        });
+      }
+    }
+
+    return errorData;
+  }
+
+  /**
+   * Form submit handler
+   */
+  function submit(e) {
+
+    var errorData = validateForm();
+
+    if(errorData) {
+      e.preventDefault();
+    }
 
     if(options.showSummary) {
-      showSummary(errors);
+      showSummary(errorData);
     }
 
     if(options.showIndividualFormErrors) {
-      showIndividualFormErrors(errors);
+      showIndividualFormErrors(errorData);
     }
 
+  }
+
+  /**
+   * Keyup handler
+   */
+  function keyup(e) {
+    var errorData = validateForm();
+
+    // We don't want to start flagging errors to the user until they have
+    // at least attempted to enter a value into a field. This allows them to
+    // tab around the form as much as they like to begin with and requried
+    // fields will only be validated when they have entered something
+    if(e.delegateTarget.value.length > 0) {
+      e.delegateTarget.isDirty = true;
+    }
+
+    if(e.delegateTarget.isDirty && options.showIndividualFormErrors) {
+      showIndividualFormErrors(errorData, e.delegateTarget);
+    }
   }
 
   /**
@@ -101,9 +155,15 @@ function Validator(element, config) {
   /**
    * Individual form errors
    */
-  function showIndividualFormErrors(errors) {
+  function showIndividualFormErrors(errors, restrictTo) {
+
     // Remove any previous form element errors
     [].forEach.call(element.querySelectorAll('.error-message'), function(el) {
+
+      if(restrictTo && el.previousSibling !== restrictTo) {
+        return;
+      }
+
       var formGroup = closest(el, '.form-group');
       formGroup.classList.remove('error');
 
@@ -115,6 +175,10 @@ function Validator(element, config) {
       // Flag each element that has an error
       errors.forEach(function(error) {
         var target = document.getElementById(error.id);
+
+        if(restrictTo && target !== restrictTo) {
+          return;
+        }
 
         var message = domify(options.individualErrorTemplate.render(error));
         target.parentNode.insertBefore(message, target.nextSibling);
