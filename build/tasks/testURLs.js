0ac24d25ab31ede51fba68c57ba1233d16e3c002
Andy Mantell <andymantell@hotmail.com>
2016-02-24 09:38:30 +0000

    Script to generate reference renderings of each component

--- build/tasks/testURLs.js
+++ build/tasks/testURLs.js
@@ -2,24 +2,28 @@ var components = require('../../build/modules/components');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 
-components.getComponents()
+module.exports = components.getComponents()
   .then(function(components) {
-    var urls = [];
+    return new Promise(function(resolve, reject) {
+      var urls = [];
 
-    components.forEach(function(component) {
-      for(variant in component.variants) {
-        if(component.variants.hasOwnProperty(variant)) {
+      components.forEach(function(component) {
+        for(variant in component.variants) {
+          if(component.variants.hasOwnProperty(variant)) {
 
-          urls.push('http://localhost:3000/components/' + component.id + '/' + variant);
-        }
-      }
-    });
-
-    mkdirp('.tmp', function() {
-      fs.writeFile('.tmp/testURLs.json', JSON.stringify(urls), function(err) {
-        if(err) {
-          console.log(err);
+            urls.push('http://localhost:3000/components/' + component.id + '/' + variant);
+          }
         }
       });
+
+      mkdirp('.tmp', function() {
+        fs.writeFile('.tmp/testURLs.json', JSON.stringify(urls), function(err) {
+          if(err) {
+            console.log(err);
+          }
+        });
+      });
+
+      resolve(urls);
     });
   });
