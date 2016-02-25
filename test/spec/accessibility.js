8bc8bf40746202f6afb8b8703d0d7c574a0a4e1d
Andy Mantell <andymantell@hotmail.com>
2016-02-25 11:04:32 +0000

    Ignoring accessibility warning that requires no action

--- test/spec/accessibility.js
+++ test/spec/accessibility.js
@@ -23,7 +23,8 @@ describe('The pattern library page at', function() {
       'WCAG2AA.Principle1.Guideline1_3.1_3_1.H39.3.NoCaption',
       'WCAG2AA.Principle1.Guideline1_3.1_3_1.H73.3.NoSummary',
       'WCAG2AA.Principle1.Guideline1_3.1_3_1.H42',
-      'WCAG2AA.Principle1.Guideline1_3.1_3_1.H49.B'
+      'WCAG2AA.Principle1.Guideline1_3.1_3_1.H49.B',
+      'WCAG2AA.Principle1.Guideline1_3.1_3_1.H48'
     ]
   });
 
