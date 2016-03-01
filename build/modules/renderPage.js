bf02b906a6cb7c9289d44f8cec5879f75b7cac6f
Andy Mantell <andymantell@hotmail.com>
2016-03-01 15:50:23 +0000

    Remove reference to non existent script

--- build/modules/renderPage.js
+++ build/modules/renderPage.js
@@ -35,7 +35,6 @@ var renderPage = function(hbs, data) {
       ].join('\n'),
       licenceMessage: '<p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated</p>',
       bodyEnd: [
-        '<!--[if lte IE 8]><script src="/javascripts/ie8-polyfills.js"></script><![endif]-->'
       ]
     };
 
