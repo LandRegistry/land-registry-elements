2e9776f2b866036db48a18e0c6e352fc43c1a584
Andy Mantell <andymantell@hotmail.com>
2016-03-09 16:35:18 +0000

    Fixing build script to allow passing of config

--- build/doBuild.js
+++ build/doBuild.js
@@ -4,20 +4,22 @@ var path = require('path');
 
 var build = require('./build');
 
-var results = new Promise(function(resolve, reject) {
-  build()
-    .then(function(directory) {
-      console.timeEnd('Total build time');
-      console.log('Assets built to:', directory);
+var results = function(config) {
+  return new Promise(function(resolve, reject) {
+    build(config)
+      .then(function(directory) {
+        console.timeEnd('Total build time');
+        console.log('Assets built to:', directory);
 
-      resolve(directory);
-    })
-    .catch(function(err) {
-      require('trace');
-      require('clarify');
-      console.trace(err);
-      reject(err);
-    });
-});
+        resolve(directory);
+      })
+      .catch(function(err) {
+        require('trace');
+        require('clarify');
+        console.trace(err);
+        reject(err);
+      });
+  });
+}
 
 module.exports = results;
--- build/tasks/javascript.js
+++ build/tasks/javascript.js
@@ -67,7 +67,7 @@ function compileJavaScript(config) {
               // and use the vanilla browserify module
               if(config.mode === 'production') {
                 b = browserify(browserfyConfig);
-                b.transform({ global: true }, 'uglifyify');
+                b.transform({ global: true }, require('uglifyify'));
               } else {
                 // Otherwise use debug mode
                 browserfyConfig.debug = true;
