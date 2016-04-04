738ef5a9e1f1e1122565b50d39c332e2797ecd7e
Andy Mantell <andymantell@hotmail.com>
2016-04-04 11:01:17 +0100

    IE9 polyfills

--- src/elements/land-registry/polyfills/ie8/controller.js
+++ src/elements/land-registry/polyfills/ie8/controller.js
@@ -9,7 +9,6 @@ require('./javascript/Array.prototype.indexOf');
 require('./javascript/Array.prototype.map');
 require('./javascript/Array.prototype.reduce');
 require('./javascript/Date.now');
-require('./javascript/Element.classList');
 require('./javascript/Function.prototype.bind');
 require('./javascript/Object.defineProperties');
 require('./javascript/Object.defineProperty');
---
+++ src/elements/land-registry/polyfills/ie9/controller.js
@@ -0,0 +1,1 @@
+require('./javascript/Element.classList');
