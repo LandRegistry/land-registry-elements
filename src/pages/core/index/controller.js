edc41085057cd39492221bfc19e14d2b5e2d30ba
Andy Mantell <andymantell@hotmail.com>
2016-04-12 09:13:20 +0100

    Fix eslint errors

--- src/pages/core/index/controller.js
+++ src/pages/core/index/controller.js
@@ -7,32 +7,32 @@ if(form) {
   var buildUrl = form.querySelectorAll('.build-url');
 
   delegate(form, 'input[name^="components"]', 'change', change);
+}
 
-  function change() {
-    // Get an array of all the components
-    var values = [].map.call(checkboxes, function(el) {
-      if(el.checked) {
-        return el.id;
-      } else {
-        return false;
-      }
-    });
+function change() {
+  // Get an array of all the components
+  var values = [].map.call(checkboxes, function(el) {
+    if(el.checked) {
+      return el.id;
+    } else {
+      return false;
+    }
+  });
 
-    // And filter out any that weren't checked
-    values = values.filter(function(value) {
-      return !!value;
-    });
+  // And filter out any that weren't checked
+  values = values.filter(function(value) {
+    return !!value;
+  });
 
-    // Build up a query string
-    var queryString = '?';
+  // Build up a query string
+  var queryString = '?';
 
-    values.forEach(function(value, index) {
-      queryString += 'components[' + value + ']=on&';
-    });
+  values.forEach(function(value) {
+    queryString += 'components[' + value + ']=on&';
+  });
 
-    // And stick it in the input fields on the index page
-    [].forEach.call(buildUrl, function(el) {
-      el.value = window.location + 'build' + queryString;
-    });
-  }
+  // And stick it in the input fields on the index page
+  [].forEach.call(buildUrl, function(el) {
+    el.value = window.location + 'build' + queryString;
+  });
 }
