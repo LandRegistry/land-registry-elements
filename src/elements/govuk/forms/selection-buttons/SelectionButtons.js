f131baa789aff6c6f1ee7f4d1a853430a8f01003
Andy Mantell <andymantell@hotmail.com>
2016-04-12 13:50:09 +0100

    Adding in vanilla js port of the gov uk selection buttons

---
+++ src/elements/govuk/forms/selection-buttons/SelectionButtons.js
@@ -0,0 +1,98 @@
+var closest = require('closest');
+var extend = require('extend');
+
+function SelectionButtons(element, config) {
+
+  var options = {
+    selectedClass: 'selected',
+    focusedClass: 'focused'
+  };
+
+  extend(options, config);
+
+  // Private variables
+  var label;
+  var form;
+
+  /**
+   * Set everything up
+   */
+  function create() {
+
+    // Bail out if we don't have the proper element to act upon
+    if (!element) {
+      return;
+    }
+
+    label = closest(element, 'label');
+    form = closest(element, 'form');
+
+    if(element.checked) {
+      markSelected();
+    }
+
+    element.addEventListener('click', clickHandler);
+    element.addEventListener('focus', focusHandler);
+    element.addEventListener('blur', focusHandler);
+  }
+
+  function markFocused(state) {
+    if (state === 'focused') {
+      label.classList.add(options.focusedClass);
+    } else {
+      label.classList.remove(options.focusedClass);
+    }
+  }
+
+  function markSelected() {
+    var radioName;
+
+    if(element.type === 'radio') {
+      radioName = element.name;
+
+      var radioGroup = form.querySelectorAll('input[name="' + radioName + '"]');
+
+      [].forEach.call(radioGroup, function(radio) {
+        closest(radio, 'label').classList.remove(options.selectedClass);
+      });
+
+      label.classList.add(options.selectedClass);
+
+    } else { // checkbox
+      if (element.checked) {
+        label.classList.add(options.selectedClass);
+      } else {
+        label.classList.remove(options.selectedClass);
+      }
+    }
+  }
+
+  function clickHandler(e) {
+    markSelected();
+  }
+
+  function focusHandler(e) {
+    var state = (e.type === 'focus') ? 'focused' : 'blurred';
+
+    markFocused(state);
+  }
+
+  /**
+   * Tear everything down again
+   */
+  function destroy() {
+    element.removeEventListener('click', clickHandler);
+    element.removeEventListener('focus', focusHandler);
+    element.removeEventListener('blur', focusHandler);
+  }
+
+  var self = {
+    create: create,
+    destroy: destroy
+  };
+
+  return self;
+
+}
+
+module.exports = SelectionButtons;
