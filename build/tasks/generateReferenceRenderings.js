33cf42827b6f5b079468412f44078d080e4ec021
Andy Mantell <andymantell@hotmail.com>
2016-06-03 13:48:19 +0100

    Tweak reference rendering script to cater for components that dont exist on master

--- build/tasks/generateReferenceRenderings.js
+++ build/tasks/generateReferenceRenderings.js
@@ -7,7 +7,8 @@ var sanitize = require('sanitize-filename');
 var url = require('url');
 var trim = require('trim-character');
 var extend = require('extend');
-var throat = require('throat')(2)
+var throat = require('throat')(3)
+var request = require('superagent');
 
 require('../server');
 
@@ -17,7 +18,7 @@ mkdirp.sync('test/fixtures/visual-regression/reference-renderings');
 var options = {
   takeShotOnCallback: true,
   timeout: 30000,
-  errorIfStatusIsNot200: true,
+  errorIfStatusIsNot200: false,
   shotSize: {
     width: 'all',
     height: 'all'
@@ -52,38 +53,57 @@ require('./testURLs')
       // Desktop screenshot
       promises.push(throat(function() {
         return new Promise(function(resolve, reject) {
-          var renderStream = webshot('http://land-registry-elements.herokuapp.com' + componentUrl, options);
+          // Check the component exists on the reference url first
+          request
+            .get('http://land-registry-elements.herokuapp.com' + componentUrl)
+            .end(function(err, res){
+              if(res.status === 200) {
+                var renderStream = webshot('http://land-registry-elements.herokuapp.com' + componentUrl, options);
 
-          var file = fs.createWriteStream('test/fixtures/visual-regression/reference-renderings/desktop-' + fileName + '.png', {encoding: 'binary'});
+                var file = fs.createWriteStream('test/fixtures/visual-regression/reference-renderings/desktop-' + fileName + '.png', {encoding: 'binary'});
 
-          renderStream.on('data', function(data) {
-            file.write(data.toString('binary'), 'binary');
-          });
+                renderStream.on('data', function(data) {
+                  file.write(data.toString('binary'), 'binary');
+                });
 
-          renderStream.on('end', function() {
-            console.log('Desktop screenshot taken for', componentUrl);
+                renderStream.on('end', function() {
+                  console.log('Desktop screenshot taken for', componentUrl);
 
-            resolve();
-          });
+                  resolve();
+                });
+              } else {
+                resolve();
+              }
+            });
         });
       }));
 
       // Mobile screenshot
       promises.push(throat(function() {
         return new Promise(function(resolve, reject) {
-          var renderStream = webshot('http://land-registry-elements.herokuapp.com' + componentUrl, mobileOptions);
+          // Check the component exists on the reference url first
+          request
+            .get('http://land-registry-elements.herokuapp.com' + componentUrl)
+            .end(function(err, res){
+              if(res.status === 200) {
+                var renderStream = webshot('http://land-registry-elements.herokuapp.com' + componentUrl, mobileOptions);
 
-          var file = fs.createWriteStream('test/fixtures/visual-regression/reference-renderings/mobile-' + fileName + '.png', {encoding: 'binary'});
+                var file = fs.createWriteStream('test/fixtures/visual-regression/reference-renderings/mobile-' + fileName + '.png', {encoding: 'binary'});
 
-          renderStream.on('data', function(data) {
-            file.write(data.toString('binary'), 'binary');
-          });
+                renderStream.on('data', function(data) {
+                  file.write(data.toString('binary'), 'binary');
+                });
 
-          renderStream.on('end', function() {
-            console.log('Mobile screenshot taken for', componentUrl);
+                renderStream.on('end', function() {
+                  console.log('Mobile screenshot taken for', componentUrl);
 
-            resolve();
-          });
+                  resolve();
+                });
+
+              } else {
+                resolve();
+              }
+            });
         });
       }));
 
