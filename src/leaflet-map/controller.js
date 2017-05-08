aaca51ae3ee3ce3bcfd2406c227a794d70bf0590
Andy Mantell <andymantell@hotmail.com>
2017-05-08 12:05:46 +0100

    Paring back the package.json

--- src/leaflet-map/controller.js
+++
@@ -1,8 +0,0 @@
-require('leaflet');
-require('proj4leaflet');
-
-/*global L:true*/
-/*global assetPath:true*/
-
-// specify the path to the leaflet images folder
-L.Icon.Default.imagePath = assetPath + '/images/leaflet/';
