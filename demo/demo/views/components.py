6809a42eb32b0bfdff9c1542dec648e9f8d9feae
Andy Mantell <andymantell@hotmail.com>
2017-06-05 12:29:39 +0100

    Linking the index page to component demo pages

--- demo/demo/views/components.py
+++ demo/demo/views/components.py
@@ -1,11 +1,25 @@
 from flask import Blueprint
 from flask import render_template
+from glob import glob
+from os import path
 
 
 # This is the blueprint object that gets registered into the app in blueprints.py.
 components = Blueprint('example', __name__)
 
 
+
 @components.route("/")
 def index():
-    return render_template('app/index.html')
+    def parse_path(demo_path):
+        return path.relpath(demo_path, 'src').replace('/demos', '').replace('.html', '')
+
+    demos = glob('src/**/demos/*.html')
+    parsed_demos = list(map(parse_path, demos))
+
+    return render_template('app/index.html', demos=parsed_demos)
+
+
+@components.route('/<component_name>/<demo_name>')
+def component_demo(component_name, demo_name):
+    return render_template('land-registry-elements/{}/demos/{}.html'.format(component_name, demo_name))
