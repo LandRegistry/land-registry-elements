9c02aaca5a6ee7ba056730e9e92d31b61215c51e
Andy Mantell <andymantell@hotmail.com>
2017-06-06 16:14:04 +0100

    linting fixes

--- src/support/poltergeist-extensions/clientside-form-validation-pubsub.js
+++ src/support/poltergeist-extensions/clientside-form-validation-pubsub.js
@@ -1,13 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener('DOMContentLoaded', function () {
   window.listOfErrorsForCucumberTests = []
-  window.PubSub.subscribe("clientside-form-validation.invalid", function(msg, data) {
+  window.PubSub.subscribe('clientside-form-validation.invalid', function (msg, data) {
     window.listOfErrorsForCucumberTests = data.errors
   })
 
   window.listOfIndividualErrorsForCucumberTests = []
-  window.PubSub.subscribe("clientside-form-validation.error", function(msg, data) {
+  window.PubSub.subscribe('clientside-form-validation.error', function (msg, data) {
     window.listOfIndividualErrorsForCucumberTests.push(data.error)
   })
-
-});
+})
